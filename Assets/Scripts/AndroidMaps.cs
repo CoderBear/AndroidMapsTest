c863bceb5d453f9c46df29c7026be41c7d6bd5ed
Joe Ortega <joeortega79@gmail.com>
2014-06-23 12:14:30 -0700

    Initial Commit

---
+++ Assets/Scripts/AndroidMaps.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidMaps : MonoBehaviour {
+
+	public static AndroidJavaClass ViewJavaClass;
+
+	// Use this for initialization
+	void Start () {
+		if(Application.platform == RuntimePlatform.Android) {
+			// Initialize Android View
+			ViewJavaClass = new AndroidJavaClass("com.tandosbs.maps.MapActivity");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnGUI() {
+		if (GUI.Button(new Rect(20, 600, 300, 240), "Show android Screen")) {
+			showMap();
+		}
+	}
+	
+	private void showMap() {
+		AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject activity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+		activity.Call("showAndroidView");
+	}
+}
